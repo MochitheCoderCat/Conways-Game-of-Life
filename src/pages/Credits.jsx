c0cef34d258f6d30cd2882f17e3594acc7868112
Shan533 <790219068@qq.com>
2024-03-18 16:14:38 -0700

    add credits page

---
+++ src/pages/Credits.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+function Credits() {
+  return (
+    <div>
+      <Navbar />
+      <div>Credits</div>
+    </div>
+  );
+}
+
+export default Credits;
