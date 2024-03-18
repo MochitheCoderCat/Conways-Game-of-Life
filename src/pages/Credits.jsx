f346a61b1c1b53d07c6e7ddd1567c2bf9176c18b
Shan533 <790219068@qq.com>
2024-03-18 16:48:22 -0700

    add content to credits page

--- src/pages/Credits.jsx
+++ src/pages/Credits.jsx
@@ -5,7 +5,23 @@ function Credits() {
   return (
     <div>
       <Navbar />
-      <div>Credits</div>
+      <div className="container">
+        <h1>Credits</h1>
+        <ul>
+          <li className="names">
+            <span>Qianyun Wang: </span>
+            <a href="https://github.com/MochitheCoderCat/Conways-Game-of-Life.git">
+              Github
+            </a>
+          </li>
+          <li className="names">
+            <span>Shanshan Wu: </span>
+            <a href="https://github.com/Shan533/Shanshan-Wu-Qianyun-Wang-assignment2.git">
+              Github
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
